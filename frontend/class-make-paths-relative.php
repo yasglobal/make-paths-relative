893bde51d0de020c2917811489777131d68f9a37
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2021-07-16 16:44:33 +0500

    Fix issue on Post Edit

--- frontend/class-make-paths-relative.php
+++ frontend/class-make-paths-relative.php
@@ -3,8 +3,23 @@
  * @package MakePathsRelative
  */
 
+/**
+ * Main Class that actually make the URLs relative.
+ */
 final class Make_Paths_Relative_Frontend {
+	/**
+	 * Site URL, Either get from the Plugin settings (If exist) otherwise WP
+	 * default `site_url()`.
+	 *
+	 * @var string
+	 */
   private $site_url = null;
+
+	/**
+	 * Site URL without HTTP Protocol.
+	 *
+	 * @var string
+	 */
   private $make_paths_relative_url = null;
 
   /**
@@ -18,7 +33,8 @@ final class Make_Paths_Relative_Frontend {
 
     $host_name = site_url();
     if ( isset( $make_relative_paths['site_url'] )
-      && ! empty( $make_relative_paths['site_url'] ) ) {
+      && ! empty( $make_relative_paths['site_url'] )
+		) {
       $host_name = $make_relative_paths['site_url'];
     }
     $this->site_url = $host_name;
@@ -36,16 +52,14 @@ final class Make_Paths_Relative_Frontend {
   }
 
   /**
-   * It makes the permalinks, scripts, styles and image URLs(src) to relative
+   * It makes the permalinks, scripts, styles and image URLs(src) to relative.
    *
    * @access public
    * @since 0.2
    *
-   * @param string $link
-   *   Permalink which is going to be shown
+   * @param string $link Permalink which is going to be shown
    *
-   * @return string $relative_link
-   *   Return the Relative Permalink
+   * @return string Return the Relative Permalink
    */
   public function make_paths_relative_remove( $link ) {
     // Don't do anything if the current query is for a feed.
@@ -79,26 +93,31 @@ final class Make_Paths_Relative_Frontend {
   }
 
   /**
-   * It converts the Permalinks to be relative for post and custom post type
+   * It converts the Permalinks to be relative for post and custom post type.
    *
    * @access public
    * @since 0.6
    *
-   * @param string $link
-   *   Permalink which is going to be shown
-   * @param object $post
-   *   Post Details
-   * @param string $leavename
-   *   keep the post name or page name
+   * @param string $link      Permalink which is going to be shown.
+   * @param object $post      Post Details.
+   * @param string $leavename keep the post name or page name.
    *
-   * @return string $relative_link
-   *   Return the Relative Permalink
+   * @return string Return the Relative Permalink.
    */
   public function relative_post_urls( $link, $post, $leavename = false ) {
     // Don't do anything if the current query is for a feed.
     if ( is_feed() ) {
       return $link;
     }
+
+		/*
+		 * If URL contains `post name` or `page name` then return the `link` as it
+		 * is to avoid conflict on Post Edit.
+		 * @link: https://wordpress.org/support/topic/compatibility-issue-between-yoast-seo-and-make-paths-relative/
+		 */
+		if ( $leavename ) {
+			return $link;
+		}
 
     $current_post_type = get_post_type();
     if ( isset( $current_post_type ) && ! empty( $current_post_type ) ) {
@@ -135,13 +154,13 @@ final class Make_Paths_Relative_Frontend {
   }
 
   /**
-   * Apply the appropriate Filters according to the configured settings
+   * Apply the appropriate Filters according to the configured settings.
    *
    * @access private
    * @since 0.2
    *
-   * @param array $make_relative_paths
-   *   Options which is selected in the from Plugin Settings Page
+   * @param array $make_relative_paths Options which is selected in the from
+	 *                                   Plugin Settings Page.
    *
    * @return void
    */
@@ -152,14 +171,17 @@ final class Make_Paths_Relative_Frontend {
     ) {
       $url_path = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
       // Check if current requested path contains `sitemap.xml`.
-      if ( isset( $url_path ) && strpos( $url_path, 'sitemap.xml' ) !== FALSE ) {
+      if ( isset( $url_path )
+				&& strpos( $url_path, 'sitemap.xml' ) !== FALSE
+			) {
         return;
       }
     }
 
     // Filters to make the permalinks to relative.
     if ( isset( $make_relative_paths['post_permalinks'] )
-      && ! empty( $make_relative_paths['post_permalinks'] ) ) {
+      && ! empty( $make_relative_paths['post_permalinks'] )
+		) {
       add_filter( 'the_permalink',
         array( $this, 'make_paths_relative_remove' ), 100
       );
@@ -175,7 +197,8 @@ final class Make_Paths_Relative_Frontend {
     }
 
     if ( isset( $make_relative_paths['page_permalinks'] )
-      && ! empty( $make_relative_paths['page_permalinks'] ) ) {
+      && ! empty( $make_relative_paths['page_permalinks'] )
+		) {
       add_filter( 'page_link',
         array( $this, 'make_paths_relative_remove' ), 100
       );
@@ -185,21 +208,24 @@ final class Make_Paths_Relative_Frontend {
     }
 
     if ( isset( $make_relative_paths['archive_permalinks'] )
-      && ! empty( $make_relative_paths['archive_permalinks'] ) ) {
+      && ! empty( $make_relative_paths['archive_permalinks'] )
+		) {
       add_filter( 'get_archives_link',
         array( $this, 'make_paths_relative_remove' ), 100
       );
     }
 
     if ( isset( $make_relative_paths['author_permalinks'] )
-      && ! empty( $make_relative_paths['author_permalinks'] ) ) {
+      && ! empty( $make_relative_paths['author_permalinks'] )
+		) {
       add_filter( 'author_link',
         array( $this, 'make_paths_relative_remove' ), 100
       );
     }
 
     if ( isset( $make_relative_paths['category_permalinks'] )
-      && ! empty( $make_relative_paths['category_permalinks'] ) ) {
+      && ! empty( $make_relative_paths['category_permalinks'] )
+		) {
       add_filter( 'term_link',
         array( $this, 'make_paths_relative_remove' ), 100, 3
       );
@@ -207,14 +233,16 @@ final class Make_Paths_Relative_Frontend {
 
     // Filters to make the scripts and style urls to relative.
     if ( isset( $make_relative_paths['scripts_src'] )
-      && ! empty( $make_relative_paths['scripts_src'] ) ) {
+      && ! empty( $make_relative_paths['scripts_src'] )
+		) {
       add_filter( 'script_loader_src',
         array( $this, 'make_paths_relative_remove' ), 100
       );
     }
 
     if ( isset( $make_relative_paths['styles_src'] )
-      && ! empty( $make_relative_paths['styles_src'] ) ) {
+      && ! empty( $make_relative_paths['styles_src'] )
+		) {
       add_filter( 'style_loader_src',
         array( $this, 'make_paths_relative_remove' ), 100
       );
@@ -222,7 +250,8 @@ final class Make_Paths_Relative_Frontend {
 
     // Filter to make the media(image) src to relative.
     if ( isset( $make_relative_paths['image_paths'] )
-      && ! empty( $make_relative_paths['image_paths'] ) ) {
+      && ! empty( $make_relative_paths['image_paths'] )
+		) {
       add_filter( 'wp_get_attachment_url',
         array( $this, 'make_paths_relative_remove' ), 100
       );
@@ -238,14 +267,11 @@ final class Make_Paths_Relative_Frontend {
    * @access public
    * @since 0.3
    *
-   * @param string $image_srcset
-   *   Source set of image
+   * @param string $image_srcset Source set of image.
    *
-   * @return string $image_srcset
-   *   Return the Relative Source set of image
+   * @return string Return the Relative Source set of image.
    */
   public function make_paths_relative_remove_srcset( $image_srcset ) {
-
     // Don't do anything if the current query is for a feed.
     if ( is_feed() ) {
       return $image_srcset;
@@ -257,7 +283,8 @@ final class Make_Paths_Relative_Frontend {
         get_option( 'make_paths_relative_exclude' )
       );
       if ( isset( $get_exclude_post_types['post_types'][$current_post_type] )
-        && $get_exclude_post_types['post_types'][$current_post_type] == "on" ) {
+        && $get_exclude_post_types['post_types'][$current_post_type] == "on"
+			) {
         return $image_srcset;
       }
     }
@@ -288,15 +315,14 @@ final class Make_Paths_Relative_Frontend {
    * @access public
    * @since 0.5.1
    *
-   * @param string $post_permalink
-   *   Absolute or Relative Permalink
+   * @param string $post_permalink Absolute or Relative Permalink.
    *
-   * @return string $post_permalink
-   *   Return Absolute Permalink
+   * @return string Return Absolute Permalink.
    */
   public function sitemap_post_url( $post_permalink ) {
     if ( strpos( $post_permalink, $this->site_url ) === false
-      && isset( $post_permalink[0] ) && $post_permalink[0] == '/' ) {
+      && isset( $post_permalink[0] ) && $post_permalink[0] == '/'
+		) {
       return $this->site_url . $post_permalink;
     }
     return $post_permalink;
